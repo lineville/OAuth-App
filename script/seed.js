345e765b6a7a1d50d45169472696530ec677a6de
Liam Neville <lineville@vassar.edu>
2018-07-23 12:22:38 -0400

    -pretiffied a few logs

--- script/seed.js
+++ script/seed.js
@@ -3,36 +3,20 @@
 const db = require('../server/db')
 const {User} = require('../server/db/models')
 
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
-
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
+
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
   console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`~~~~~~~~~~~~~~~~~~
+               seeded successfully
+               ~~~~~~~~~~~~~~~~~~~`)
 }
 
-// We've separated the `seed` function from the `runSeed` function.
-// This way we can isolate the error handling and exit trapping.
-// The `seed` function is concerned only with modifying the database.
 async function runSeed() {
   console.log('seeding...')
   try {
--- server/index.js
+++ server/index.js
@@ -97,7 +97,9 @@ const createApp = () => {
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                  Listening on PORT: ${PORT} ...
+                  http://localhost:${PORt} `)
   )
 
   // set up our socket control center
